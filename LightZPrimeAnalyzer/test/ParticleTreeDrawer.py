5b14efe2b086aa5c6e223ccd1d3428a3c14493af
SridharaDasu <dasu@hep.wisc.edu>
2016-02-26 16:47:49 -0600

    Added arguments for convenience

--- LightZPrimeAnalyzer/test/ParticleTreeDrawer.py
+++ LightZPrimeAnalyzer/test/ParticleTreeDrawer.py
@@ -4,17 +4,25 @@ process = cms.Process('ParticleTreeDrawer')
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing()
+options.register('inputFile', 'file:EDMFileWithGenParticles.root', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'Input file')
+options.register('sourceTag', 'genParticles', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'genParticle input tag name')
+options.register('maxEvents', 1, VarParsing.multiplicity.singleton, VarParsing.varType.int, 'Maximum number of events')
+options.parseArguments()
+
 process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:/hdfs/store/user/tuanqui/ZprimeM10_STEP2-step2/step2-step1.root'))
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(options.inputFile))
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
-    src = cms.InputTag("genParticles"),
+    src = cms.InputTag(options.sourceTag),
     printP4 = cms.untracked.bool(False),
     printPtEtaPhi = cms.untracked.bool(True),
     printVertex = cms.untracked.bool(False),
