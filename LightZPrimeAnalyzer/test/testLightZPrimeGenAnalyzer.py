06545134b5a29d45f969cb09197168b26ff4385a
SridharaDasu <dasu@hep.wisc.edu>
2016-03-05 01:57:10 -0600

    Removed unnecessary frontier access - removes junk line in output

--- LightZPrimeAnalyzer/test/testLightZPrimeGenAnalyzer.py
+++ LightZPrimeAnalyzer/test/testLightZPrimeGenAnalyzer.py
@@ -5,15 +5,6 @@ process = cms.Process('LightZPrimeGenAnalyzer')
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
 
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load("Configuration.Geometry.GeometryRecoDB_cff")
-
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
-process.load("Configuration.StandardSequences.MagneticField_cff")
-
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
 
