81cf5cfc569cdc938a097bcbc8c90d14bf5cfd04
Usama Hussain <uhussain@wisc.edu>
2016-08-30 14:01:04 +0200

    Cleaning up

--- JetAnalyzer/python/ggMETFilters_cff.py
+++ JetAnalyzer/python/ggMETFilters_cff.py
@@ -9,21 +9,6 @@ globalTightHalo2016Filter.taggingMode = cms.bool(True)
 from RecoMET.METFilters.primaryVertexFilter_cfi import *
 primaryVertexFilter.vertexCollection = cms.InputTag('offlinePrimaryVertices')
 
-#primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
-#                                   vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-#                                   minimumNDOF = cms.uint32(4) ,
-#                                   maxAbsZ = cms.double(24),
-#                                   maxd0 = cms.double(2),
-#                                   filter = cms.bool(False)
-#                                   )
-
-#goodVertices = cms.EDFilter(
-#  "VertexSelector",
-#  filter = cms.bool(True),
-#  src = cms.InputTag("offlinePrimaryVertices"),
-#  cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.rho < 2")
-#)
-
 from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import *
 HBHENoiseFilterResultProducer.minZeros = cms.int32(99999)
 HBHENoiseFilterResultProducer.IgnoreTS4TS5ifJetInLowBVRegion=cms.bool(False) 
