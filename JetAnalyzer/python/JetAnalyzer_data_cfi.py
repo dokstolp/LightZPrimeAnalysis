c979a30e910cee64ced8d8ecfdb19457195bc5ab
Usama Hussain <uhussain@wisc.edu>
2016-11-29 18:25:13 +0100

    Different jet corrector chains for Data and MC respectively

--- JetAnalyzer/python/JetAnalyzer_data_cfi.py
+++ JetAnalyzer/python/JetAnalyzer_data_cfi.py
@@ -6,10 +6,10 @@ process.load('JetMETCorrections.Configuration.JetCorrectors_cff')
 
 process.ak4PFchsCorrectedJets   = cms.EDProducer('CorrectedPFJetProducer',
     src         = cms.InputTag('ak4PFJetsCHS'),
-    correctors  = cms.VInputTag('ak4PFCHSL1FastL2L3Corrector')
+    correctors  = cms.VInputTag('ak4PFCHSL1FastL2L3ResidualCorrector')
     )
 
-process.jetSequence = cms.Sequence(process.ak4PFCHSL1FastL2L3CorrectorChain*process.ak4PFchsCorrectedJets)
+process.jetSequence = cms.Sequence(process.ak4PFCHSL1FastL2L3ResidualCorrectorChain*process.ak4PFchsCorrectedJets)
 
 JetAnalyzer = cms.EDAnalyzer('JetAnalyzer',
 	jetsTag = cms.InputTag("ak4PFchsCorrectedJets"),
